7e4e7e50f7d423b6195d80179f6837bcc99c1dcb
Jayden Arquelada <jayden.arquelada@mail.utoronto.ca>
2017-07-11 04:48:39 +0000

    Removed Assignment folder, which previously held the code.

--- Assignment/src/com/bank/generics/AccountTypes.java
+++
@@ -1,5 +0,0 @@
-package com.bank.generics;
-
-public enum AccountTypes {
-  CHEQUING, SAVING, TFSA;
-}
--- Assignment/src/com/bank/generics/Roles.java
+++
@@ -1,5 +0,0 @@
-package com.bank.generics;
-
-public enum Roles {
-  ADMIN, TELLER, CUSTOMER;
-}
