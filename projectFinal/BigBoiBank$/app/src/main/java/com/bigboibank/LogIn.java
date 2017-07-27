a960086c99e2dc8dae5ee15759dce31182c3bc25
Jayden Arquelada <jayden.arquelada@mail.utoronto.ca>
2017-07-27 19:09:27 +0000

    Added empty android code

---
+++ projectFinal/BigBoiBank$/app/src/main/java/com/bigboibank/LogIn.java
@@ -0,0 +1,13 @@
+package com.bigboibank;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LogIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+}
