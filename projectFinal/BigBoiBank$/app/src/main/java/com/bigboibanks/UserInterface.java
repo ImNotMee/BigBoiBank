e1bd8a8ce9b04f09855fd3a1591f84adc6403514
Jayden Arquelada <jayden.arquelada@mail.utoronto.ca>
2017-07-31 19:07:53 +0000

    Colour changes

--- projectFinal/BigBoiBank$/app/src/main/java/com/bigboibanks/UserInterface.java
+++ projectFinal/BigBoiBank$/app/src/main/java/com/bigboibanks/UserInterface.java
@@ -177,7 +177,7 @@ public class UserInterface extends AppCompatActivity {
               R.array.customerOptionsArray, android.R.layout.simple_spinner_item);
     }
     // Specify the layout to use when the list of choices appears
-    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    adapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
     // Apply the adapter to the spinner
     spinner.setAdapter(adapter);
     spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
