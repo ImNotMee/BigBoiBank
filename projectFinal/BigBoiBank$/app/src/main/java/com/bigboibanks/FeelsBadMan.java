a7e96d2873395a83d17fd6509dd60914166dffd5
leperrin <leperrin@UTORONTO.CA>
2017-08-01 03:40:17 +0000

    made it so the music from krabz screen will stop after a certain time

--- projectFinal/BigBoiBank$/app/src/main/java/com/bigboibanks/FeelsBadMan.java
+++ projectFinal/BigBoiBank$/app/src/main/java/com/bigboibanks/FeelsBadMan.java
@@ -3,6 +3,7 @@ package com.bigboibanks;
 import android.content.pm.ActivityInfo;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 public class FeelsBadMan extends AppCompatActivity {
@@ -21,8 +22,25 @@ public class FeelsBadMan extends AppCompatActivity {
         https://tenor.com/view/tiny-violin-mr-krabs-spongebob-gif-3533818
          */
 
+        // end time in milliseconds
+        int end = 20000;
+
+        // stop player when run
+        final MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.mr_krabs);
+        Runnable stopPlayer = new Runnable() {
+            @Override
+            public void run() {
+                mediaPlayer.stop();
+            }
+        };
+
         setContentView(R.layout.mr_krabz);
-        MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.mr_krabs);
+        // start the player
         mediaPlayer.start();
+
+        // stop player after given time in milliseconds pass
+        Handler handler = new Handler();
+        handler.postDelayed(stopPlayer, end);
+
     }
 }
